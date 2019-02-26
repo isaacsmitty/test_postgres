92b04e01f1d095e0cdc14ada41d58cc0f51e95e1
Isaac Smith <isaac.smitty@gmail.com>
2019-02-26 17:50:51 +0000

    separate functions for getting getting data from query and outputtng data to console

--- lookup_people.js
+++ lookup_people.js
@@ -13,21 +13,30 @@ const client = new pg.Client({
 
 const input = process.argv.slice(2)[0];
 
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
-  client.query("SELECT * FROM famous_people WHERE first_name = $1::text or last_name = $1::text", [input], (err, result) => {
+
+function getPersonByName (name,callback)  {
+
+  client.connect((err) => {
     if (err) {
-      return console.error("error running query", err);
+      return console.error("Connection Error", err);
     }
+    client.query("SELECT * FROM famous_people WHERE first_name = $1::text or last_name = $1::text", [name], (err, result) => {
+      if (err) {
+        return console.error("error running query", err);
+      } else {
+        callback(result)
+      }
+    });
+  });
+}
+
+  function print(result)  {
 
     for (var i = 0; i < result.rows.length; i++) {
-
       console.log(`${i + 1}: ${result.rows[i].first_name} ${result.rows[i].last_name}, born ${result.rows[i].birthdate}`);
-    }
-
+  }
     client.end();
-  });
-});
+}
 
+
+getPersonByName(input, print);
