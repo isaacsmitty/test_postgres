e1121f8262ba514310f489e5d72c425828acd8b0
Isaac Smith <isaac.smitty@gmail.com>
2019-02-26 17:28:38 +0000

    first version of lookup, all in one function

---
+++ lookup_people.js
@@ -0,0 +1,33 @@
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+const input = process.argv.slice(2)[0];
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  client.query("SELECT * FROM famous_people WHERE first_name = $1::text or last_name = $1::text", [input], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+
+    for (var i = 0; i < result.rows.length; i++) {
+
+      console.log(`${i + 1}: ${result.rows[i].first_name} ${result.rows[i].last_name}, born ${result.rows[i].birthdate}`);
+    }
+
+    client.end();
+  });
+});
+
